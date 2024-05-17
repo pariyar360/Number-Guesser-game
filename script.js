d76989114b56e3561340a87059aeea75670a409b
Prakash <pariyar360@gmail.com>
2024-05-17 21:42:39 +1000

    final commit. script added and all working perfectly

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+  humanDifference = Math.abs(humanGuess - targetNumber);
+  computerDifference = Math.abs(computerGuess - targetNumber);
+  if (
+    humanDifference == computerDifference ||
+    humanDifference < computerDifference
+  ) {
+    return true;
+  } else if (humanDifference > computerDifference) {
+    return false;
+  }
+};
+
+const updateScore = (winner) => {
+  if (winner === "human") {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber++;
+};
